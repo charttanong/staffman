558872fae9626e4078d4a990a0933a86050b57cf
charttanong <charttanong_zza@hotmail.com>
2024-10-17 18:04:06 +0700

    Fix index.html path

--- server.js
+++ server.js
@@ -2,29 +2,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path'); // Import path to resolve file paths
 require('dotenv').config();
 
 const app = express();
+
+// Serve static files (if you have any other assets like images or CSS in a "public" folder, serve them)
+app.use(express.static(__dirname));
+
+// Routes
 const staffRoutes = require('./routes/staff');
+app.use('/api/staff', staffRoutes);
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('public')); // Serve static files from public folder
-
-// Serve index.html for the root route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Send index.html file
-});
-
-// Staff routes
-app.use('/api/staff', staffRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
+
+// Serve the index.html file from root
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 // Start the server
 const PORT = process.env.PORT || 5000;
